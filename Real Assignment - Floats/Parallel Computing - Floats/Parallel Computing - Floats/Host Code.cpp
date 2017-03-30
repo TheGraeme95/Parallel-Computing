63e0057cbbbcd0423d82f49f89b5f2fc17ba25fa
Graeme Hutchison <graeme_96@hotmail.co.uk>
2017-03-30 08:54:18 +0100

    Fully completed and commented (I think)

--- Real Assignment - Floats/Parallel Computing - Floats/Parallel Computing - Floats/Host Code.cpp
+++ Real Assignment - Floats/Parallel Computing - Floats/Parallel Computing - Floats/Host Code.cpp
@@ -11,70 +11,99 @@
 #include "Utils.h"
 #include <chrono>
 
+
 typedef float mytype;
 using namespace std;
 using namespace std::chrono;
 
-void print_help() { 
-	cerr << "Application usage:" << endl;
+cl::Event profEvent;
+size_t numInputElements, workGroupSize, initialInputSize; // Accessed by every kernel function so defined as global.
 
+void print_help() 
+{ 
+	cerr << "Application usage:" << endl;
 	cerr << "  -p : select platform " << endl;
 	cerr << "  -d : select device" << endl;
 	cerr << "  -l : list all platforms and devices" << endl;
 	cerr << "  -h : print this message" << endl;
 }
 
-//////////////////////////// SUM ///////////////////////////////////////
+// A method to run the specified by argument kernel, while also displaying the profiling information
+// every time it is called.
 
-float Sum(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, vector<mytype> inputData, size_t WorkGroupSize)
+void callKernel(cl::Kernel& name, cl::CommandQueue& queue)
 {
-	size_t workGroupSize = WorkGroupSize;	
-	size_t numInputElements = inputData.size();	
-	size_t input_size = inputData.size() * sizeof(mytype);//size in bytes	
-	vector<mytype> outputData(inputData.size());
-	size_t output_size = outputData.size() * sizeof(mytype);
-	size_t N = inputData.size()/workGroupSize;
+	cl::Event profEvent;
+	queue.enqueueNDRangeKernel(name, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize), NULL, &profEvent);
+	queue.finish();
+	std::cout << "Execution time: "
+		<< profEvent.getProfilingInfo<CL_PROFILING_COMMAND_END>() -
+		profEvent.getProfilingInfo<CL_PROFILING_COMMAND_START>() << "ns" << "\n\t"
+		<< GetFullProfilingInfo(profEvent, ProfilingResolution::PROF_US) << '\n' << std::endl;
+}
 
+//////////////////////////// SUM KERNEL CALL ///////////////////////////////////////
+
+
+float Sum(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, vector<mytype>& inputData)
+{
+	size_t input_size = inputData.size() * sizeof(mytype); // Size of input in bytes 
+	vector<mytype> outputData(inputData.size()); // Defining vector to contain the output data.
+	size_t output_size = outputData.size() * sizeof(mytype); // Size of output in bytes.
+	size_t N = inputData.size()/workGroupSize; // The amount of partial sums returned after one iteration of reduction.
+
+
+	// Creating 2 buffers in which to pass and recieve information to and from kernel.
 	cl::Buffer inBuffer(context, CL_MEM_READ_WRITE, input_size);
 	cl::Buffer outBuffer(context, CL_MEM_READ_WRITE, output_size);
 
+	// Write data to the input buffer. 
 	queue.enqueueWriteBuffer(inBuffer, CL_TRUE, 0, input_size, &inputData[0]);
+	// Fill the output buffer with 0's to allocate memory.
 	queue.enqueueFillBuffer(outBuffer, 0, 0, output_size);	
 
+
+	// Create the kernel for the sum function.
+	// Passing the input buffer, local memory size and output buffer as arguments.
 	cl::Kernel kernel_sum = cl::Kernel(program, "floatSum");
 	kernel_sum.setArg(0, inBuffer);
 	kernel_sum.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));//local memory size	
 	kernel_sum.setArg(2, outBuffer);			
 	
 	
-	//1st Reduction
-	queue.enqueueNDRangeKernel(kernel_sum, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));	
-	
+	// The kernel is called once and then the output buffer is set to the input of the next
+	// kernel call to further reduce.
+	callKernel(kernel_sum, queue);	
 	kernel_sum.setArg(0, outBuffer);
 
+	// It will continue to call the kernel until all the partial sums returned by reduction can be encompassed
+	// within one work group.
+	// Bigger work group sizes can potentially increase performance due to less kernel calls.
 	while(N > workGroupSize)
 	{
-		queue.enqueueNDRangeKernel(kernel_sum, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
-		N = N/workGroupSize;
-		cout << N << endl;
+		callKernel(kernel_sum, queue);
+		N = N/workGroupSize;		
 	}
 
-	queue.enqueueNDRangeKernel(kernel_sum, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
+	// Once the amount of partial sums in the output array is less then the work group size it will
+	// then run once more to return a full reduced sum.
+	callKernel(kernel_sum, queue);
 
-	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);
-	
+	// The output buffer is read into the output vector.
+	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);	
+
+	// The first value of the output array contains the sum of the whole input array which is
+	// then returned.
 	return (float)outputData[0];
-	
 
 }
 
 //////////////////////////// MINIMUM and MAXIMUM ///////////////////////////////////////
 
-float MiniumumMaximum(cl::Context& context, cl::CommandQueue& queue , cl::Program& program, vector<mytype> inputData, size_t WorkGroupSize, int choice)
+float MinimumMaximum(cl::Context& context, cl::CommandQueue& queue , cl::Program& program, vector<mytype>& inputData, int choice)
 {
-	size_t workGroupSize = WorkGroupSize;	
-	size_t numInputElements = inputData.size();	
-	size_t input_size = inputData.size() * sizeof(mytype);//size in bytes
+	
+	size_t input_size = inputData.size() * sizeof(mytype);
 	vector<mytype> outputData(inputData.size());
 	size_t output_size = outputData.size() * sizeof(mytype);
 	size_t N = inputData.size() / workGroupSize;
@@ -85,57 +114,121 @@ float MiniumumMaximum(cl::Context& context, cl::CommandQueue& queue , cl::Progra
 	queue.enqueueWriteBuffer(inBuffer, CL_TRUE, 0, input_size, &inputData[0]);
 	queue.enqueueFillBuffer(outBuffer, 0, 0, output_size);
 
-	cl::Kernel kernel_min = cl::Kernel(program, "MinMax");
-	kernel_min.setArg(0, inBuffer);
-	kernel_min.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));//local memory size	
-	kernel_min.setArg(2, outBuffer);
-	kernel_min.setArg(3, choice);
+	// Arguments are the same as the sum kernel except the choice argument is passed.
+	// If choice == 1 then it will calculate the minimum, else it will calculate the max.
+	cl::Kernel kernel_minmax = cl::Kernel(program, "MinMax");
+	kernel_minmax.setArg(0, inBuffer);
+	kernel_minmax.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));	
+	kernel_minmax.setArg(2, outBuffer);
+	kernel_minmax.setArg(3, choice);
 
-	queue.enqueueNDRangeKernel(kernel_min, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
-	kernel_min.setArg(0, outBuffer);
+	callKernel(kernel_minmax, queue);
+	kernel_minmax.setArg(0, outBuffer);
 
 	while (N > workGroupSize)
 	{
-		queue.enqueueNDRangeKernel(kernel_min, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
+		callKernel(kernel_minmax, queue);
 		N = N / workGroupSize;		
 	}
-
-	queue.enqueueNDRangeKernel(kernel_min, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
-	
-
-	
+	// Keep reducing till only the min/max of all the workgroups is returned.
+	callKernel(kernel_minmax, queue);
 
 	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);
 	
+	// Return the final min/max value.
 	return (float)outputData[0];
 
 }
 
-float StandardDeviation(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, cl::Buffer inputBuffer, size_t WorkGroupSize, float mean)
-{
-
-	size_t workGroupSize = WorkGroupSize;
-	size_t numInputElements = inputData.size();
+float StandardDeviation(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, vector<mytype>& inputData, float& mean)
+{	
 	size_t input_size = inputData.size() * sizeof(mytype);//size in bytes
-	vector<mytype> outputData(1);
+	vector<mytype> outputData(inputData.size());
 	size_t output_size = outputData.size() * sizeof(mytype);
-		
+	size_t N = inputData.size() / workGroupSize;
+	
+	cl::Buffer inBuffer(context, CL_MEM_READ_WRITE, input_size);
 	cl::Buffer outBuffer(context, CL_MEM_READ_WRITE, output_size);
 
-	
+	queue.enqueueWriteBuffer(inBuffer, CL_TRUE, 0, input_size, &inputData[0]);
 	queue.enqueueFillBuffer(outBuffer, 0, 0, output_size);
 
+	// Calls the squared differences kernel in which the mean is passed in as an argument.
+	// This is subtracted from each of the original input values, then squared and summed up
+	// to return partial sum of sqaured differences.
 	cl::Kernel kernel_std = cl::Kernel(program, "SquaredDifferences");
-	kernel_std.setArg(0, inputBuffer);
-	kernel_std.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));//local memory size	
+	kernel_std.setArg(0, inBuffer);
+	kernel_std.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));
 	kernel_std.setArg(2, outBuffer);
 	kernel_std.setArg(3, mean);
 
+	callKernel(kernel_std, queue);
 
-	queue.enqueueNDRangeKernel(kernel_std, cl::NullRange, cl::NDRange(numInputElements), cl::NDRange(workGroupSize));
+	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);
+	
+	// The partial sums returned from the SquaredDifferences kernel is then fed into the Sum function
+	// to return one total sum.
+	float sum = Sum(context, queue, program, outputData);
+	
+	// The total is then divided by the amount of elements N and then sqaured rooted to give
+	// standard deviation.
+	return sqrt((sum/(initialInputSize)));
+}
 
-	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);	
-	return (float)outputData[0];
+//////////////////////////////////// ATTEMPT AT BITONIC SORT ////////////////////////////////////////////////////////////
+
+vector<mytype> BitonicSort(cl::Context& context, cl::CommandQueue& queue, cl::Program& program, vector<mytype>& inputData)
+{
+	size_t input_size = inputData.size() * sizeof(mytype);
+	vector<mytype> outputData(inputData.size());
+	size_t output_size = outputData.size() * sizeof(mytype);
+	size_t N = inputData.size() / workGroupSize;
+	
+	cl::Buffer inBuffer(context, CL_MEM_READ_WRITE, input_size);
+	cl::Buffer outBuffer(context, CL_MEM_READ_WRITE, input_size);
+
+	queue.enqueueWriteBuffer(inBuffer, CL_TRUE, 0, input_size, &inputData[0]);
+	queue.enqueueFillBuffer(outBuffer, 0, 0, output_size);
+
+	cl::Kernel kernel_sort = cl::Kernel(program, "sort");
+	kernel_sort.setArg(0, inBuffer);
+	kernel_sort.setArg(1, cl::Local(sizeof(mytype) * workGroupSize));
+	kernel_sort.setArg(2, outBuffer);	
+	kernel_sort.setArg(3, 0); // the merge argument decide what the offset is going to be.
+
+	int merge = 1; // Start with the offsetted iD
+	callKernel(kernel_sort, queue);
+	kernel_sort.setArg(0, outBuffer); // Rerun the kernel continuously on the output buffer.
+
+	bool isSorted = false;
+	
+	while(isSorted == false) // While the vector isn't sorted, it will continue to call the kernel repeatedly
+	{
+		kernel_sort.setArg(3, merge); // Alternate between the merge value being 1 or 0.
+		merge = merge == 1 ? 0 : 1;		//Doing this allows the original element and the offset to be operated on.
+
+		callKernel(kernel_sort, queue); 
+
+		queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]); // Read out the buffer every time
+																			// to check that it has been sorted.
+		if (is_sorted(outputData.begin(), outputData.end()) == true) // If it has been sorted then stop running the kernel.
+		{
+			isSorted = true;
+		}
+		
+		
+	}
+	
+	queue.enqueueReadBuffer(outBuffer, CL_TRUE, 0, output_size, &outputData[0]);
+	
+	vector<mytype> outVec; // output vector to hold the median, upper quartile and lower quartile.
+
+	outVec.push_back(outputData[round(0.5 * initialInputSize)]);
+	outVec.push_back(outputData[round(0.75 * initialInputSize)]);
+	outVec.push_back(outputData[round(0.25 * initialInputSize)]);
+	
+
+	return outVec;
 
 }
 
@@ -147,6 +240,8 @@ int main(int argc, char **argv) {
 	int platform_id = 0;
 	int device_id = 0;
 
+	std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+	
 	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
@@ -156,12 +251,12 @@ int main(int argc, char **argv) {
 
 //////////////////// Reading the data from the file //////////////////
 
-	ifstream inFileStream("temp_lincolnshire_short.txt");	
+	ifstream inFileStream("temp_lincolnshire.txt");	
 //////////////////// OPEN FILE ////////////////////////
 	inFileStream.seekg(0, ios_base::end);
-	size_t size = inFileStream.tellg();	
-	inFileStream.seekg(0, ios_base::beg);	
-	char * data = new char[size];
+	size_t size = inFileStream.tellg();	 // Finds the size of the file
+	inFileStream.seekg(0, ios_base::beg);
+	char * data = new char[size]; // Creates an array of that size in which all data is read into as a block.
 	inFileStream.read(&data[0], size);
 //////////////////// CLOSE FILE ///////////////////////
 	inFileStream.close();
@@ -169,32 +264,32 @@ int main(int argc, char **argv) {
 	//Parse the file by keeping track of the last space before \n
 	long spacePos = 0;
 
-	//Final vector of values
+	// Vector to store values
 	vector<mytype> inputData;
 
-	for (long i = 0; i < size; ++i) 
+	for (long i = 0; i < size; ++i) // Iterates through the stored block of data, to separate and extract last column
 	{
 		char c = data[i];
 		if (c == ' ')
 		{
-			//+1 to i to itterate after last space
+			// Finds the last space which indicates the final column.
 			spacePos = i + 1;
 		}
 		else if (c == '\n')
 		{
 			int leng = i - spacePos, index = 0;
 
-			//Allocate buffer for word between space_pos and \n
-			char * word = new char[leng];
+			// Finds the word between the last space and before the new line. AKA last column data.
+			char* word = new char[leng];
 			word[leng] = '\0';
 
-			//Get every char between last space and \n
+			// Stores that data between last space and next line.
 			for (int j = spacePos; j < i; j++, index++)
 			{
 				word[index] = data[j];
 			}
 
-			//Parse word to double (higher precison)
+			// Convert string to float values which are pushed back into the storage vector.
 			inputData.push_back((strtof(word, NULL)));
 		}
 	}
@@ -202,10 +297,10 @@ int main(int argc, char **argv) {
 ///////////////////////////////////////////////////////////////////
 
 	try {
-
+		// Defining context, queue and program.
 		cl::Context context = GetContext(platform_id, device_id);
 		cout << "Runinng on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << endl;
-		cl::CommandQueue queue(context);
+		cl::CommandQueue queue(context, CL_QUEUE_PROFILING_ENABLE);
 		cl::Program::Sources sources;
 		AddSources(sources, "myKernels.cl");
 		cl::Program program(context, sources);
@@ -224,24 +319,58 @@ int main(int argc, char **argv) {
 		//Part 4 - memory allocation
 		//host - input
 		cl::Device device = context.getInfo<CL_CONTEXT_DEVICES>()[0];	
-		size_t intialInputSize = inputData.size();
-		size_t workGroupSize = 512;
-		size_t padding = inputData.size() % workGroupSize;		
+		initialInputSize = inputData.size(); // Store input size before padding to use for accurate mean.
+		workGroupSize = 1024; // Higher workgroupSize can reduces # of kernel calls potentially reducing call overhead
+		size_t padding = inputData.size() % workGroupSize;	
 
-		if (padding) 
+		if (padding) // Padding the input vector with 0's to ensure that it is a multiple of 2.
 		{			
 			std::vector<mytype> A_ext(workGroupSize - padding, 0);			
 			inputData.insert(inputData.end(), A_ext.begin(), A_ext.end());
 		}
-
-		float total = Sum(context, queue, program, inputData, workGroupSize);
-		printf("\nTotal: %f\n", total);
-
-		float Minimum = MiniumumMaximum(context, queue, program, inputData, workGroupSize, 1);
-		printf("\nMinimum: %f\n", Minimum);
+		numInputElements = inputData.size(); // Number of input elements.
 		
+		///////////////////////////////// DISPLAYING RESULTS ///////////////////////////////////////
 
+		// Each of the functions defined above return the values that are then printed here.
 
+		float total = Sum(context, queue, program, inputData);
+		float mean = total / initialInputSize;
+		printf("\nTotal: %f\n", total);
+		printf("Mean: %f\n", mean);
+
+		printf("\n##############################################################################\n");
+		printf("\n");
+		float Minimum = MinimumMaximum(context, queue, program, inputData, 1);
+		printf("\nMinimum: %f\n", Minimum);
+
+		printf("\n##############################################################################\n");
+		printf("\n");
+		float Maximum = MinimumMaximum(context, queue, program, inputData, 0);
+		printf("\nMaxiumum: %f\n", Maximum);
+
+		printf("\n##############################################################################\n");
+		printf("\n");
+		float standardDev = StandardDeviation(context, queue, program, inputData, mean);
+		printf("\nStandard Deviation: %f\n", standardDev);
+
+		printf("\n##############################################################################\n");
+		printf("\nPress key to sort..\n");
+		getchar();
+
+		vector<mytype> sorted = BitonicSort(context, queue, program, inputData);
+		float median = sorted.at(0);
+		float upperQuartile = sorted.at(1);
+		float lowerQuartile = sorted.at(2);
+		printf("\n##############################################################################\n");
+		printf("\nMedian: %f\n", median);
+		printf("Upper Quartile: %f\n", upperQuartile);
+		printf("Lower Quartile: %f\n", lowerQuartile);
+		
+		// Prints the total time the program has been running for in microseconds.
+		printf("\nTotal Time Elapsed (microseconds)\n");
+		cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count() << endl;	
+		
 			
 		}
 		catch (cl::Error err)
--- Real Assignment - Floats/Parallel Computing - Floats/Parallel Computing - Floats/myKernels.cl
+++ Real Assignment - Floats/Parallel Computing - Floats/Parallel Computing - Floats/myKernels.cl
@@ -1,59 +1,186 @@
-  __kernel void floatSum ( __global const float* input, __local int* scratch, __global float* output)
+ 
+ //////////////////////////////////////////// SUM KERNEL ////////////////////////////////////////////////
+ 
+__kernel void floatSum ( __global const float* input, __local float* localMem, __global float* output)
  {
-  uint local_id = get_local_id(0);
-  uint group_size = get_local_size(0);
+	int global_ID = get_global_id(0);
+	uint local_ID = get_local_id(0);
+	uint group_size = get_local_size(0);
 
-  // Copy from global memory to local memory
-  scratch[local_id] = input[get_global_id(0)];
-  barrier(CLK_LOCAL_MEM_FENCE);
+  //Pass the whole input array into local memory
+	localMem[local_ID] = input[global_ID];
+	barrier(CLK_LOCAL_MEM_FENCE); // Waits for everything to catch up 
 
-      // Divide WorkGroup into 2 parts and add elements 2 by 2
-      // between local_id and local_id + stride
-
-	  for (int i = 1; i < group_size; i *= 2) {
-		if (!(local_id % (i * 2)) && ((local_id + i) < group_size)){
-			//printf("scratch[%d] += scratch[%d] (%d += %d)\n", local_id, local_id + i, scratch[local_id], scratch[local_id + i]);
-			scratch[local_id] += scratch[local_id + i];
-			}
-		barrier(CLK_LOCAL_MEM_FENCE);
+	  //Each workgroup is divided into 2 parts and 2 elements from the local_ID 
+	  //and stride are added together.
+	for (int i = 1; i < group_size; i *= 2) {
+		if (!(local_ID % (i * 2)) && ((local_ID + i) < group_size))
+		{			
+			localMem[local_ID] += localMem[local_ID + i];
+		}
+		barrier(CLK_LOCAL_MEM_FENCE); //Wait for both parts of the workgroup to finish.
 	}           
 
-  // Write result into partialSums[nWorkGroups]
-  if (local_id == 0)
-    output[get_group_id(0)] = scratch[0];	
+  // When the local ID reaches 0, the resulting sum for each workgroup will be written back to the output buffer.
+  //The output buffer will contain all the resulting partial sums.
+	if (local_ID == 0)
+		output[get_group_id(0)] = localMem[0];	
   
  }
+
+
+
+///////////////////////////////////// KERNEL FOR GETTING MIN AND MAX ////////////////////////////////////////////////
+
+__kernel void MinMax(__global const float* input, __local float* localMem, __global float* output, int choice)
+{
+	int global_ID = get_global_id(0);
+	int local_ID = get_local_id(0);
+	int group_Size = get_local_size(0);
+
+
+	//Pass the input into local memory.
+	localMem[local_ID] = input[global_ID];
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	for (int i = 1; i < group_Size; i *= 2)
+	{ 
+		if (!(local_ID % (i * 2)) && ((local_ID + i) < group_Size))
+		{
+
+			//If the choice argument is 1 then it will return the miniumum value
+			//else it will return the maximum.
+			if (choice == 1)
+				{
+					//Iterate through workgroup and reduce into either the maximum or minimum.
+					if (localMem[local_ID] > localMem[local_ID + i])
+							localMem[local_ID] = localMem[local_ID + i];			
+				}
+			else
+				{
+					if (localMem[local_ID] < localMem[local_ID + i])
+							localMem[local_ID] = localMem[local_ID + i];
+				}
+		}
+
+		barrier(CLK_LOCAL_MEM_FENCE);
+
+	}
+	//For each workgroup the minimum/maximum for each workgroup is returned to the output array.
+	if (!local_ID)
+		output[get_group_id(0)] = localMem[0];	
+
+}
+
+
+///////////////////////////// RETURNING THE SQUARED DIFFERENCES /////////////////////////////////////////////////////////
+
+__kernel void SquaredDifferences(__global const float* input, __local float* localMem, __global float* output, float mean) 
+{
+	int global_ID = get_global_id(0);
+	int local_ID = get_local_id(0);
+	int group_Size = get_local_size(0);
+
+	//When passing the data into local memory, the mean is subtracted and the result is squared for each element.
+	localMem[local_ID] = ((input[global_ID] - mean) * (input[global_ID] - mean));
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	for (int i = 1; i < group_Size; i *= 2) 
+	{
+		if (!(local_ID % (i * 2)) && ((local_ID + i) < group_Size))
+		{
+			
+			localMem[local_ID] += localMem[local_ID + i];
+		}
+		barrier(CLK_LOCAL_MEM_FENCE);
+	}
+
+	// Writes out the partial sums of each workgroup, which will then be summed to give the sum of 
+	// squared differnces with the mean
+	if (!local_ID) 
+	{
+		output[get_group_id(0)] = localMem[0];	
+	}
+}
+
+/////////////////////////////////// BITONIC SORT //////////////////////////////////////////////////////////
+
+__kernel void sort(__global const float* input, __local float* localMem, __global float* output, int merge)
+{
+    int global_ID = get_global_id(0);
+    int local_ID = get_local_id(0);
+    int group_size = get_group_id(0);
+    int N = get_local_size(0);
+	
+    int offset = global_ID + ((N/2) * merge); // offset the beginning of the array
+
+	//load into local memory
+    localMem[local_ID] = input[offset];
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int l = 1; l < N; l <<= 1)
+    {
+        bool direction = ((local_ID & (l<<1)) != 0);
+
+        for (int inc = l; inc > 0; inc >>= 1)	//Bitonic merge
+        {
+            int j = local_ID ^ inc;
+            float i_data = localMem[local_ID]; // define two different directions effectively being the start and end
+            float j_data = localMem[j];			// of the vector.
+
+            bool smaller = (j_data < i_data) || ( j_data == i_data && j < local_ID); // check which bitonic is smaller
+            bool swap = smaller ^ (j < local_ID) ^ direction; // and what the direction is
+
+            barrier(CLK_LOCAL_MEM_FENCE);
+
+            localMem[local_ID] = (swap) ? j_data : i_data; // Swap the data according to whether
+            barrier(CLK_LOCAL_MEM_FENCE);
+        }
+    }
+
+	// Sync and write back to global memory
+    output[offset] = localMem[local_ID];    
+
+}
+
+
+////////////////////////////// MINIMUM KERNEL FOR INTEGERS USING ATOMIC MIN (NOT USED) /////////////////
 
  __kernel void Minimum( __global const float* input, __local int* localMem, __global float* output)
  {
 	uint local_id= get_local_id(0);
 	uint group_size = get_local_size(0);
 
-  // Copy from global memory to local memory
+    //Pass the whole input array into local memory
 	localMem[local_id] = input[get_global_id(0)];
 	barrier(CLK_LOCAL_MEM_FENCE);
 
-      // Divide WorkGroup into 2 parts and add elements 2 by 2
-      // between local_id and local_id + stride
-
+    //Each workgroup is divided into 2 parts and 2 elements from the local_ID 
+	//and stride are compared.
 	for (int i = 1; i < group_size; i *= 2) {
 		if (!(local_id % (i * 2)) && ((local_id + i) < group_size))		
 		{
 			if (localMem[local_id] > localMem[local_id + i])
-					localMem[local_id] = localMem[local_id + i];				
+					localMem[local_id] = localMem[local_id + i];		
 
 		}
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}           
 
-  // Write result into partialSums[nWorkGroups]
-  if (local_id == 0)
-    output[get_group_id(0)] = localMem[0];	
- 
- 
+  // The resulting minimums of each workgroup are then compared and the atomic min function returns
+  // the minumum of all the workgroups.
+	if (local_id == 0)
+		output[get_group_id(0)] = localMem[0];	
+	 
  }
 
-__kernel void Sum(__global const int* input, __local int* localMem, __global int* output) 
+
+
+//////////////////////////// SUM KERNEL FOR INTEGERS USING ATOMIC ADD (NOT USED) /////////////////////////////
+
+__kernel void Sum(__global const int* input, __local float* localMem, __global int* output) 
 {
 	int global_ID = get_global_id(0);
 	int local_ID = get_local_id(0);
@@ -83,57 +210,8 @@ __kernel void Sum(__global const int* input, __local int* localMem, __global int
 	}
 }
 
-__kernel void MinMax(__global const float* input, __local int* localMem, __global float* output, int choice)
-{
-	int global_ID = get_global_id(0);
-	int local_ID = get_local_id(0);
-	int group_Size = get_local_size(0);
-
-	localMem[local_ID] = input[global_ID];
-
-	barrier(CLK_LOCAL_MEM_FENCE);
-
-	for (int i = 1; i < group_Size; i *= 2)
-	{ 
-		if (!(local_ID % (i * 2)) && ((local_ID + i) < group_Size))
-		{
-			if (choice == 1)
-				{
-					if (localMem[local_ID] > localMem[local_ID + i])
-							localMem[local_ID] = localMem[local_ID + i];			
-				}
-			else
-				{
-					if (localMem[local_ID] < localMem[local_ID + i])
-							localMem[local_ID] = localMem[local_ID + i];
-				}
-		}
-
-		barrier(CLK_LOCAL_MEM_FENCE);
-
-	}
-
-	if (!local_ID)
-		output[get_group_id(0)] = localMem[0];	
 
 
-}
 
 
-__kernel void SquaredDifferences(__global const int* input, __local int* localMem, __global int* output, float mean)
-{
-	
-	int global_ID = get_global_id(0);
-	int local_ID = get_local_id(0);
-	int group_Size = get_local_size(0);	
 
-	localMem[local_ID] = input[global_ID];
-	barrier(CLK_LOCAL_MEM_FENCE);
-
-	localMem[local_ID] = ((localMem[local_ID] - mean)*(localMem[local_ID] - mean));
-	
-
-	atomic_add(&output[0], localMem[local_ID]);
-
-
-}
